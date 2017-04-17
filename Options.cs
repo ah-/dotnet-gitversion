37ba2d2a64948f3355add3386f688622ca531242
Connie Yau <conniey@microsoft.com>
2017-04-17 14:01:19 -0700

    Add Configuration Options

---
+++ Options.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace dotnet.gitversion
+{
+    internal class Options
+    {
+        private static readonly IDictionary<string, string> _commandMapping =
+            new Dictionary<string, string> {
+            { "-j", "projectjson" },
+            { "-c", "csproj" },
+            { "-h", "help" },
+            { "-?", "help" },
+        };
+
+        public string ProjectJson { get; set; }
+        public string CsProj { get; set; }
+        public bool Help { get; set; }
+
+        public static string GetHelpMessage()
+        {
+            var builder = new StringBuilder("USAGE: dotnet gitversion <options>");
+            builder.AppendLine("    If no options are specified, then it will look for the project.json or *.csproj in current directory.");
+            builder.AppendLine();
+            builder.AppendLine("OPTIONS:");
+
+            foreach (var kvp in _commandMapping)
+            {
+                if (string.Equals(kvp.Value, "help", StringComparison.OrdinalIgnoreCase))
+                {
+                    builder.AppendLine($"{kvp.Key}|--{kvp.Value}");
+                }
+                else
+                {
+                    builder.AppendLine($"{kvp.Key}|--{kvp.Value} PathToFile");
+                }
+            }
+
+            return builder.ToString();
+        }
+
+        public bool NoFilesSpecified
+        {
+            get { return string.IsNullOrEmpty(CsProj) && string.IsNullOrEmpty(ProjectJson); }
+        }
+
+        public static Options Parse(string[] args)
+        {
+            var builder = new ConfigurationBuilder()
+                .AddCommandLine(args, _commandMapping);
+
+            var configure = new ConfigureFromConfigurationOptions<Options>(builder.Build());
+            var options = new Options();
+
+            configure.Action(options);
+            return options;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,19 +1,40 @@
-﻿using System;
-using System.IO;
-using GitVersion;
+﻿using GitVersion;
 using GitVersion.Helpers;
 using Newtonsoft.Json.Linq;
+using System;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Xml.Linq;
 
-namespace ConsoleApplication
+namespace dotnet.gitversion
 {
     public class Program
     {
-        public static void Main(string[] args)
+        private const string ProjectJsonFile = "project.json";
+        private const string CsprojFile = ".csproj";
+        private static readonly Regex s_regex = new Regex(@"(?<json>project\.json)|(?<csproj>\.csproj$)");
+
+        public static int Main(string[] args)
         {
             Logger.SetLoggers(
                 Console.WriteLine,
                 Console.WriteLine,
                 Console.WriteLine);
+
+            var options = Options.Parse(args);
+            if (options.Help)
+            {
+                Console.WriteLine(Options.GetHelpMessage());
+                return 0;
+            }
+
+            if (options.NoFilesSpecified && !TryFindNetCoreProject(out options))
+            {
+                Console.Error.WriteLine($"Could not locate {ProjectJsonFile} or {CsprojFile} in current directory and no command line arguments were specified.");
+                Console.WriteLine(Options.GetHelpMessage());
+                return -1;
+            }
 
             var fs = new FileSystem();
             var gv = new GitVersion.ExecuteCore(fs);
@@ -22,10 +43,85 @@ namespace ConsoleApplication
             var version = gv.ExecuteGitVersion(null, null, auth, null, false, ".", null);
             Console.WriteLine($"Setting version: {version.LegacySemVerPadded}");
 
-            var filename = "project.json";
-            var projectJson = JObject.Parse(File.ReadAllText(filename));
-            projectJson["version"] = version.LegacySemVerPadded;
-            File.WriteAllText(filename, projectJson.ToString());
+            if (!string.IsNullOrEmpty(options.ProjectJson))
+            {
+                UpdateProjectJson(options.ProjectJson, version.LegacySemVerPadded);
+            }
+
+            if (!string.IsNullOrEmpty(options.CsProj))
+            {
+                UpdateCsproj(options.CsProj, version.LegacySemVerPadded);
+            }
+
+            return 0;
+        }
+
+        private static bool TryFindNetCoreProject(out Options options)
+        {
+            options = new Options();
+            var found = false;
+
+            foreach (var file in new DirectoryInfo(Directory.GetCurrentDirectory()).GetFiles())
+            {
+                var match = s_regex.Match(file.Name);
+
+                if (match.Success)
+                {
+                    found = true;
+
+                    if (match.Groups["json"].Success)
+                    {
+                        options.ProjectJson = file.FullName;
+                    }
+                    else if (match.Groups["csproj"].Success)
+                    {
+                        options.CsProj = file.FullName;
+                    }
+                }
+            }
+
+            return found;
+        }
+
+        private static void UpdateProjectJson(string projectJsonFile, string legacySemVerPadded)
+        {
+            var projectJson = JObject.Parse(File.ReadAllText(projectJsonFile));
+            projectJson["version"] = legacySemVerPadded;
+            File.WriteAllText(projectJsonFile, projectJson.ToString());
+        }
+
+        /// <summary>
+        /// Looks for msbuild element PackageVersion to update with the name.
+        /// https://docs.microsoft.com/en-us/nuget/schema/msbuild-targets
+        /// </summary>
+        /// <param name="csproj"></param>
+        /// <param name="legacySemVerPadded"></param>
+        private static void UpdateCsproj(string csproj, string legacySemVerPadded)
+        {
+            var document = XDocument.Load(csproj);
+            var packageVersion = XName.Get("PackageVersion");
+            var isSet = false;
+            var descendents = document.Root.Descendants().ToArray();
+            foreach (var node in document.Root.Descendants(packageVersion))
+            {
+                node.Value = legacySemVerPadded;
+                isSet = true;
+            }
+
+            if (!isSet)
+            {
+                var propertyGroup = new XElement("PropertyGroup");
+                var version = new XElement(packageVersion)
+                {
+                    Value = legacySemVerPadded
+                };
+
+                propertyGroup.Add(version);
+
+                document.Root.AddFirst(propertyGroup);
+            }
+
+            document.Save(File.OpenWrite(csproj));
         }
     }
 }
